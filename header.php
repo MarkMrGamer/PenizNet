9846ca731247ba312305955348b4e2a6b7ba1bd6
Sniped <77207659+smplybored@users.noreply.github.com>
2021-01-23 19:40:41 -0300

    Fixed header so it doesn't depend on the root directory

--- header.php
+++ header.php
@@ -16,7 +16,7 @@ $username = $_SESSION['username'];
 $query = "SELECT * FROM users WHERE username = '$username'";
 $result = $mysqli->query($query);
 $nav = $result->fetch_assoc();
-echo "<img src=\"images/icons/coins.png\" style=\"float:right;\" title=\"Penies\"><span style=\"float:right;\">" . $nav['money'] . "</span>";
+echo "<img src=\"/images/icons/coins.png\" style=\"float:right;\" title=\"Penies\"><span style=\"float:right;\">" . $nav['money'] . "</span>";
 }
 ?>
 </td>
@@ -26,11 +26,11 @@ echo "<img src=\"images/icons/coins.png\" style=\"float:right;\" title=\"Penies\
 <?php
 if(isset($_SESSION['username'])) {
 ?>
-<img src="images/icons/house.png"><a href="home.php">Home</a> | <img src="images/icons/comment.png"><a href="chat.php">Chat</a> | <img src="images/icons/user.png"><a href="users.php">Users</a> | <img src="images/icons/chart_bar.png"><a href="/stocks">Stocks</a> | <img src="images/icons/email.png"><a href="mail.php">Mail(<?php echo $nav['mail']; ?>)</a> | <img src="images/icons/key.png"><a href="logout.php">Log Out</a>
+<img src="/images/icons/house.png"><a href="/home.php">Home</a> | <img src="/images/icons/comment.png"><a href="/chat.php">Chat</a> | <img src="/images/icons/user.png"><a href="/users.php">Users</a> | <img src="/images/icons/chart_bar.png"><a href="/stocks">Stocks</a> | <img src="/images/icons/email.png"><a href="/mail.php">Mail(<?php echo $nav['mail']; ?>)</a> | <img src="/images/icons/key.png"><a href="logout.php">Log Out</a>
 <?php
 }else{ 
 ?>
-<img src="images/icons/house.png"><a href="home.php">Home</a> | <img src="images/icons/comment.png"><a href="chat.php">Chat</a> | <img src="images/icons/user.png"><a href="users.php">Users</a> | <img src="images/icons/chart_bar.png"><a href="/stocks">Stocks</a> | <img src="images/icons/key.png"><a href="login.php">Login</a>
+<img src="/images/icons/house.png"><a href="/home.php">Home</a> | <img src="images/icons/comment.png"><a href="/chat.php">Chat</a> | <img src="/images/icons/user.png"><a href="/users.php">Users</a> | <img src="/images/icons/chart_bar.png"><a href="/stocks">Stocks</a> | <img src="images/icons/key.png"><a href="/login.php">Login</a>
 <?php
 }
 ?>
