0b1dc5db422c1edf817eb36fe336b52ba49b6875
MarkMrGamer <30990201+MarkMrGamer@users.noreply.github.com>
2021-01-23 16:54:37 -0800

    Add files via upload
    fixed a huge vuln on the manage.php

--- manage.php
+++ manage.php
@@ -17,16 +17,18 @@ header('Location: /');
 <tr>
 <td>
 <h3>Manage Profile</h3>
+<form method="POST" action="update.php" enctype="multipart/form-data">
 <br>
-<form method="POST" action="update.php">
 <address>About Me:</address>
 <textarea name="about" style="width:300px;height:100px;"><?php echo $nav['about']; ?></textarea>
 <br><br>
 <address>Profile Background:</address>
-<input type="text" name="background" value="<?php echo $nav['background']; ?>">
+  <input type="file" name="image" id="image" accept="image/x-png,image/gif,image/jpeg" />
+  <input type="submit" value="Update Background" name="file">
 <br><br>
 <address>Profile Picture:</address>
-<input type="text" name="photo" value="<?php echo $nav['photo']; ?>">
+  <input type="file" name="image2" id="image2" accept="image/x-png,image/gif,image/jpeg" />
+  <input type="submit" value="Update Picture" name="file2">
 <br><br>
 <input type="submit" value="Update">
 </form>
--- update.php
+++ update.php
@@ -8,14 +8,57 @@ header('Location: /peniznet');
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
     // variables
-    $about = htmlspecialchars($_POST['about']);
-    $background = addslashes($_POST['background']);
-	$photo = addslashes($_POST['photo']);
+	$about = htmlspecialchars($_POST['about']);
     $username = $_SESSION['username'];
 	
-        //start updating
-        $query1 = $mysqli->prepare("UPDATE users SET about = ?, background = ?, photo = ? WHERE username = ?");
-        $query1->bind_param('ssss', $about, $background, $photo, $username);
+	if(isset($_POST["file"])) {
+    $target_file = $_FILES['image']['name'];
+	$file_type=$_FILES['image']['type'];
+	$file_tmp =$_FILES['image']['tmp_name'];
+    $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+	$file1 = "images/uploads/". $target_file;
+    $extensions= array("jpeg","jpg","png","gif");
+    $temp = explode(".", $_FILES["image"]["name"]);
+    $newfilename = round(microtime(true)) . '.' . end($temp);
+	$file1 = "images/uploads/" . $newfilename;
+	 
+      if(in_array($file_ext,$extensions)=== false){
+		 die("extension not allowed, please choose a JPEG,GIF or PNG file.");
+      } else {
+        $query1 = $mysqli->prepare("UPDATE users SET background = ? WHERE username = ?");
+        $query1->bind_param('ss', $file1, $username);
+        $query1->execute();
+        $query1->close();
+		move_uploaded_file($file_tmp,"images/uploads/".$newfilename);
+        die("updated your profile background!");
+	  }
+	}
+	
+	if(isset($_POST["file2"])) {
+    $target_file = $_FILES['image2']['name'];
+	$file_type=$_FILES['image2']['type'];
+	$file_tmp =$_FILES['image2']['tmp_name'];
+    $file_ext=strtolower(end(explode('.',$_FILES['image2']['name'])));
+	$file2 = "images/uploads/". $target_file;
+    $extensions= array("jpeg","jpg","png","gif");
+    $temp = explode(".", $_FILES["image2"]["name"]);
+    $newfilename = round(microtime(true)) . '.' . end($temp);
+	$file2 = "images/uploads/" . $newfilename;
+	
+      if(in_array($file_ext,$extensions)=== false){
+		 die("extension not allowed, please choose a JPEG,GIF or PNG file.");
+      } else {
+        $query1 = $mysqli->prepare("UPDATE users SET photo = ? WHERE username = ?");
+        $query1->bind_param('ss', $file2, $username);
+        $query1->execute();
+        $query1->close();
+		move_uploaded_file($file_tmp,"images/uploads/".$newfilename);
+        die("updated your profile picture!");
+	  }
+	}
+	
+        $query1 = $mysqli->prepare("UPDATE users SET about = ? WHERE username = ?");
+        $query1->bind_param('ss', $about, $username);
         $query1->execute();
         $query1->close();
         die("updated your profile!");
